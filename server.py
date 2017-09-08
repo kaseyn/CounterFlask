c1f9afcb99ffcd4adb21116ce0fb7af5eb3b9a15
Kasey_N <kasey311@gmail.com>
2017-09-08 14:20:10 -0700

    Initial Commit

---
+++ server.py
@@ -0,0 +1,22 @@
+from flask import Flask, render_template, request, redirect, session
+app = Flask(__name__)
+app.secret_key = 'ThisIsSecret'
+
+
+@app.route("/")
+def index():
+	session["count"]
+	session["count"] += 1
+	return render_template("index.html", count_html=session['count'])
+
+@app.route("/plus_two")
+def two():
+	session["count"] +=1
+	return redirect("/")
+
+@app.route("/count_reset")
+def reset():
+	session["count"] = 0
+	return redirect("/")
+
+app.run(debug=True)
